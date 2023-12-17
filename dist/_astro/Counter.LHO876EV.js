994dc76e4b3a7037d46edd9972638a56aadbf639
Tihomir Selak <tknox.dr@gmail.com>
2023-12-17 16:18:24 +0100

    BG-01: Add first solid theme and build

---
+++ dist/_astro/Counter.LHO876EV.js
@@ -0,0 +1,1 @@
+import{c as a,g as i,a as c,i as $,t as g}from"./web.0UFuaSkr.js";const u=g("<div>Count: <!$><!/>"),m=()=>{const[e,n]=a(0);return setInterval(()=>n(e()+1),1e3),(()=>{const t=i(u),r=t.firstChild,s=r.nextSibling,[l,o]=c(s.nextSibling);return $(t,e,l,o),t})()};export{m as Counter};
