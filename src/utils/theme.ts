ec6278c9394a5937c361e21b5b99421990e18aca
Tihomir Selak <tknox.dr@gmail.com>
2023-12-19 10:53:04 +0100

    BG-02: Add theme switcher

---
+++ src/utils/theme.ts
@@ -0,0 +1,9 @@
+export const getTheme = () => {
+  if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+    return localStorage.getItem("theme");
+  }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return "dark";
+  }
+  return "light";
+};
